05ee70ae27a88132f04deb639d4e06510486c430
Aleksandr Riabov <zkelo@yandex.ru>
2023-05-23 10:40:44 +0300

    Added `ApiServerErrorException`

---
+++ src/exceptions/ApiServerErrorException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace devtek\sdk\exceptions;
+
+use Exception;
+
+/**
+ * API server error exception
+ *
+ * @version 1.0.0
+ */
+class ApiServerErrorException extends Exception
+{
+    //
+}
